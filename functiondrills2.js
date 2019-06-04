45c7827b75c7a8fb33c7cb20fb58f00b150e7dcb
geneggy <geneggy@gmail.com>
2019-06-03 21:43:31 -0700

    finished all drills

--- functiondrills2.js
+++ functiondrills2.js
@@ -64,3 +64,63 @@ messageArr.map(function(word) {
     return ' ';
   }
 });
+
+function daysInMonth(month, leapYear) {
+  switch (month) {
+  case 'February':
+    if (!leapYear) {
+      return 'February has 28 days';
+    } else{
+      return 'February has 29 days';
+    }
+  case 'April':
+  case 'June':
+  case 'September':
+  case 'November':
+    return  `${month} has 30 days`;
+  case 'January':
+  case 'March':
+  case 'May':
+  case 'July':
+  case 'August':
+  case 'October':
+  case 'December':
+    return `${month} has 31 days`;
+  }
+}
+
+daysInMonth('February', true);
+// not sure if breaks are needed, ask why
+
+function rockPaperScissors(num) {
+  const randomNo = Math.floor(Math.random() * 3) + 1;
+  if (num < 1 || num > 3){
+    throw Error;
+  } else if (num === 1) {
+    if (randomNo === 1){
+      return 'Tie';
+    }else if(randomNo === 2){
+      return 'Lose';
+    } else {
+      return 'Win';
+    }
+  } else if (num === 2) {
+    if (randomNo === 2){
+      return 'Tie';
+    }else if(randomNo === 1){
+      return 'Lose';
+    } else {
+      return 'Win';
+    }
+  } else if (num === 3) {
+    if (randomNo === 3){
+      return 'Tie';
+    }else if(randomNo === 2){
+      return 'Lose';
+    } else {
+      return 'Win';
+    }
+  }
+}
+  
+rockPaperScissors(1);
