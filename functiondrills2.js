7d69d3c9724aedc8fab33767aa83fcf15d619063
geneggy <geneggy@gmail.com>
2019-06-03 17:36:21 -0700

    finished cracking code

--- functiondrills2.js
+++ functiondrills2.js
@@ -45,4 +45,22 @@ function decode(word) {
   }
 }
 
-decode('chicken');
+let message = 'craft block argon meter bells brown croon droop';
+const messageArr = message.split(' ');
+messageArr.map(function(word) {
+  if(word.charAt(0) === 'a') {
+    return word.charAt(1);   
+  }
+  else if(word.charAt(0) === 'b') {
+    return word.charAt(2);   
+  }
+  else if(word.charAt(0) === 'c') {
+    return word.charAt(3);   
+  }
+  else if(word.charAt(0) === 'd') {
+    return word.charAt(4);   
+  }
+  else {
+    return ' ';
+  }
+});
